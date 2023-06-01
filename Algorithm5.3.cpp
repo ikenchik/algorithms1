0d9b11fa7380fcd91f38e979559adfe389887987
ikenchik <125026649+ikenchik@users.noreply.github.com>
2023-06-02 02:25:11 +0300

    Add files via upload

---
+++ Algorithm5.3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string encrypt(string text) {
+    string key = "юукенгшщзхъфавыпролджэячсмитьбц";
+    string encrypted = "";
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] == ' ') {
+            encrypted += ' ';
+        }
+        else {
+            int index = text[i] - 'а';
+            encrypted += key[index];
+        }
+    }
+    return encrypted;
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string text = "люблю животных";
+    string encrypted = encrypt(text);
+    cout << "Исходный текст: " << text << endl;
+    cout << "Зашифрованный текст: " << encrypted << endl;
+    return 0;
+}
