20e47daf217d7e63a7ade8d8d9880bd8952b7a11
ikenchik <125026649+ikenchik@users.noreply.github.com>
2023-06-02 01:27:44 +0300

    Add files via upload

---
+++ Algorithm3.1.cpp
@@ -0,0 +1,12 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> arr = { 4, 2, 7, 5, 1, 6, 3 };
+    make_heap(arr.begin(), arr.end(), greater<int>());
+    for (int x : arr) {
+        cout << x << " ";
+    }
+    return 0;
+}
