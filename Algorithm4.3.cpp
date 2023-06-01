3f23fbb044d8cbf89b717cd746801e1c160b8bbe
ikenchik <125026649+ikenchik@users.noreply.github.com>
2023-06-02 02:17:42 +0300

    Add files via upload

---
+++ Algorithm4.3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+void countChars(string str) {
+    unordered_map<char, int> charCount;
+    for (char c : str) {
+        if (charCount.find(c) == charCount.end()) { 
+            charCount[c] = 1;
+        }
+        else {
+            charCount[c]++;
+        }
+    }
+    for (auto p : charCount) {
+        cout << p.first << ": " << p.second << endl;
+    }
+}
+int main() {
+    string str;
+    setlocale(LC_ALL, "rus");
+    cout << "Ввод строки: ";
+    getline(cin, str);
+    countChars(str);
+    return 0;
+}
